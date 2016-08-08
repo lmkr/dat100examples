dd83c5bab3301c7902f1f21f92fe6a70a2334f69
lmkr <lmkr@HiB-20379.hib.no>
2016-08-08 14:32:59 +0200

    Lagt til eksempel med simpel dialogboks

---
+++ HelloWorldDialog/src/no/hib/dat100/HelloWorldDialog.java
@@ -0,0 +1,12 @@
+package no.hib.dat100;
+
+import static javax.swing.JOptionPane.*;
+
+public class HelloWorldDialog {
+
+	public static void main(String[] args) {
+		showMessageDialog(null, "Velkommen til DAT100");
+
+	}
+
+}
