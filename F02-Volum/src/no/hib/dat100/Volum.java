3aa24cc347b3bd2863a055fa45cc9809f4dc97f2
Harald Soleim <harald.soleim@hib.no>
2016-08-23 10:48:54 +0200

    ok

---
+++ F02-Volum/src/no/hib/dat100/Volum.java
@@ -0,0 +1,22 @@
+package no.hib.dat100;
+
+public class Volum {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int lengde = 3;
+		int bredde = 4;
+		int hoyde = 5;
+		int volum = lengde * bredde * hoyde;
+		System.out.println("Volumet er: " + volum);
+		lengde = 4;
+		bredde = 5;
+		hoyde = 6;
+		volum = lengde * bredde * hoyde;
+		System.out.println("Volumet er: " + volum);
+
+	}
+
+}
