ecd2a149f65f483f19f122c79c0dc6c37a5df456
Harald Soleim <harald.soleim@hib.no>
2016-08-30 08:16:27 +0200

    Bruk av switch

---
+++ F04-Karakter/src/no/hib/dat100/Karakter.java
@@ -0,0 +1,37 @@
+package no.hib.dat100;
+
+import java.util.Scanner;
+
+public class Karakter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner tastatur = new Scanner(System.in);
+		System.out.print("Oppgi en karakter A..F [STORE BOKSTAVER] ");
+		String karakter = tastatur.nextLine();
+		switch (karakter) {
+		case "A":
+			System.out.println("Svært bra");
+			break;
+		case "B":
+			System.out.println(" Meget bra");
+			break;
+		case "C":
+			System.out.println("Bra");
+		case "D":
+			System.out.println("Noe bra");
+			break;
+		case "E":
+			System.out.println("Lite bra");
+		case "F":
+			System.out.println("Ikke bra");
+			break;
+		default:
+			System.out.println("ugyldig karakter");
+		}
+
+	}
+
+}
