a3e7373d2cd5d947592c8a5ae364e9522baff89d
Harald Soleim <harald.soleim@hib.no>
2016-08-30 08:37:02 +0200

    switch, dialogboks

---
+++ F04-KarakterD/src/no/hib/dat100/Karakter.java
@@ -0,0 +1,37 @@
+package no.hib.dat100;
+
+import static javax.swing.JOptionPane.*;
+
+public class Karakter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String melding = "";
+		String karakter = showInputDialog("Oppgi en karakter A..F [STORE BOKSTAVER] ");
+
+		switch (karakter) {
+			case "A":
+				melding = "Svært bra";
+				break;
+			case "B":
+				melding = "Meget bra";
+				break;
+			case "C":
+				melding = "Bra";
+			case "D":
+				melding = "Noe bra";
+				break;
+			case "E":
+				melding = "Lite bra";
+			case "F":
+				melding = "Ikke bra";
+				break;
+			default:
+				melding = "ugyldig karakter";
+		}
+		showMessageDialog(null, melding);
+	}
+
+}
