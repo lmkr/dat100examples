028f0cbe92b4575df3482ef75c4d32d3ff48f6a0
Harald Soleim <harald.soleim@hib.no>
2016-09-09 12:43:53 +0200

    Kvadrattall metoder

--- F07-Kvadrattall/src/no/hib/dat100/Tall.java
+++ F07-Kvadrattall/src/no/hib/dat100/Tall.java
@@ -1,16 +1,12 @@
 package no.hib.dat100;
 
-public class Tall  {
-		  public static void main(String[] args) {
-		    Kalkulator r = new Kalkulator(); // Oppretter et objekt
-		    double[] roetter = r.beregnTiRoetter();//returenerer en tabell av tall
+public class Tall {
+	public static void main(String[] args) {
+		Kalkulator r = new Kalkulator(); // Oppretter et objekt
+		double[] roetter = r.beregnTiRoetter();// returenerer en tabell av tall
 
-		    for (int k = 0; k < 10; k++){
-		       System.out.println(roetter[k]); //skriver ut innholdet av tabellen
-		   }  
-		  }//main
-		}// class
-
-
-
-
+		for (int k = 0; k < 10; k++) {
+			System.out.println(roetter[k]); // skriver ut innholdet av tabellen
+		}
+	}// main
+}// class
