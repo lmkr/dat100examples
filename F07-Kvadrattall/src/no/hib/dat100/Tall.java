b562e1ce53e70bb936cc57710b305698e257000a
Harald Soleim <harald.soleim@hib.no>
2016-09-11 19:02:42 +0200

    formatert utskrift

--- F07-Kvadrattall/src/no/hib/dat100/Tall.java
+++ F07-Kvadrattall/src/no/hib/dat100/Tall.java
@@ -3,7 +3,7 @@ package no.hib.dat100;
 public class Tall {
 	public static void main(String[] args) {
 		Kalkulator r = new Kalkulator(); // Oppretter et objekt
-		double[] roetter = r.beregnTiRoetter();// returenerer en tabell av tall
+		double[] roetter = r.beregnTiRoetter();// returnerer en tabell av tall
 
 		for (int k = 0; k < 10; k++) {
 			// System.out.println(roetter[k]); // skriver ut innholdet av
