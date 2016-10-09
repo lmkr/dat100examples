5f6fafb0de7fcc5b951bdefc22beedb366f5b7b0
soh <soh@HIB-10392.hib.no>
2016-10-09 19:20:48 +0200

    Konstruktørar i arvehierarki

--- F14 - Dyr - Pattedyr - Hund/src/no/hib/dat100/Dyr.java
+++ F14 - Dyr - Pattedyr - Hund/src/no/hib/dat100/Dyr.java
@@ -1,17 +1,18 @@
 package no.hib.dat100;
 
-public class Dyr {
-    
-    public Dyr() {
-        System.out.println("Dyr()");
-    }
 
-    public Dyr(String parameter) {
-        System.out.println("Dyr(\"" + parameter + "\")");
-    }
-        
-    public Dyr(String p1, String p2) {
-        System.out.println("Dyr(\"" + p1 + "\", \"" + p2 + "\")");
-    }
-        
+public class Dyr {
+
+	public Dyr() {
+		System.out.println("Dyr()");
+	}
+
+	public Dyr(String parameter) {
+		System.out.println("Dyr(\"" + parameter + "\")");
+	}
+
+	public Dyr(String p1, String p2) {
+		System.out.println("Dyr(\"" + p1 + "\", \"" + p2 + "\")");
+	}
+
 }
--- F14 - Dyr - Pattedyr - Hund/src/no/hib/dat100/Main.java
+++ F14 - Dyr - Pattedyr - Hund/src/no/hib/dat100/Main.java
@@ -2,19 +2,19 @@ package no.hib.dat100;
 
 public class Main {
 
-    public static void main(String[] args) {
-        
-        strek();
-        Hund fido = new Hund("fido");
-        strek();
-        
-        strek();
-        Hund nn = new Hund();
-        strek();
-                
-    }
-    
-    private static void strek() {
-        System.out.println("------------------------------");
-    }
+	public static void main(String[] args) {
+
+		strek();
+		Hund fido = new Hund("fido");
+		strek();
+
+		strek();
+		Hund nn = new Hund();
+		strek();
+
+	}
+
+	private static void strek() {
+		System.out.println("------------------------------");
+	}
 }
