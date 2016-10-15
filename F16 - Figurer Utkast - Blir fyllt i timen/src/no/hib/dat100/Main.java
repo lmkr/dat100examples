1f012a67eff654adac3ce0d04b4ba057c42a69d5
soh <soh@HIB-10392.hib.no>
2016-10-15 15:47:44 +0200

    Utkast. Skal jobbe vidare med eksempelet i timen.

---
+++ F16 - Figurer Utkast - Blir fyllt i timen/src/no/hib/dat100/Main.java
@@ -0,0 +1,10 @@
+package no.hib.dat100;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ F16 - Figurer Utkast - Blir fyllt i timen/src/no/hib/dat100/Rektangel.java
@@ -0,0 +1,5 @@
+package no.hib.dat100;
+
+public class Rektangel {
+
+}
