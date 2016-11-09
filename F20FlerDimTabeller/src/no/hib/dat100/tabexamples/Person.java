14d7a876fb5fe3ec798e9cd745816fd8146bd547
Lars Kristensen <lmkr@hib.no>
2016-11-09 12:30:38 +0100

    Endre toString metode for Person til ikke å bruke newline

--- F20FlerDimTabeller/src/no/hib/dat100/tabexamples/Person.java
+++ F20FlerDimTabeller/src/no/hib/dat100/tabexamples/Person.java
@@ -14,7 +14,7 @@ public class Person {
 	public String toString() {
 		
 		return 
-				"navn  : " + navn + "\n" +
+				"navn  : " + navn + " " +
 				"epost : " + epost;
 	}
 }
--- F20FlerDimTabeller/src/no/hib/dat100/tabexamples/ToDimExample.java
+++ F20FlerDimTabeller/src/no/hib/dat100/tabexamples/ToDimExample.java
@@ -10,10 +10,9 @@ public class ToDimExample {
 		personer[0][1] = new Person("B", "b@person.com");
 		personer[0][2] = new Person("C", "c@person.com");
 		personer[1][0] = new Person("D", "d@person.com");
-		personer[2][1] = new Person("E", "d@person.com");
-		personer[3][2] = new Person("F", "f@person.com");
+		personer[1][1] = new Person("E", "d@person.com");
+		personer[1][2] = new Person("F", "f@person.com");
 
 		// iterer over personer og skriv ut info for hver person
 	}
-
 }
